8b6c86f1fcc5bb12e611c5391772eabb9f0b6543
Brandon Dunbar <brandon.dunbar97@gmail.com>
2021-08-03 01:17:03 -0500

    Delete SamBotSim directory

--- SamBotSim/Source/SamBotSim/SamBotSim.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SamBotSim.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SamBotSim, "SamBotSim" );
